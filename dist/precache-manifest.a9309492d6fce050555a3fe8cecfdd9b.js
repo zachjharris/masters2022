b28dbad18993cc72c41f08442303673886fb208b
Zach Harris <zachjharris@gmail.com>
2021-04-10 17:17:45 -0500

    build

---
+++ dist/precache-manifest.a9309492d6fce050555a3fe8cecfdd9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cc9eb6dcb63d27cade056e912c4f7e",
+    "url": "https://zachjharris.github.io/masters2021/apple-touch-icon-152x152.jpg"
+  },
+  {
+    "revision": "9ebe689afa0a9e0e3798",
+    "url": "https://zachjharris.github.io/masters2021/css/app.f7532203.css"
+  },
+  {
+    "revision": "20046d1d74a88670d29b",
+    "url": "https://zachjharris.github.io/masters2021/css/chunk-vendors.b48ffef7.css"
+  },
+  {
+    "revision": "734ed5c4896fdc9fcc127ec413d73983",
+    "url": "https://zachjharris.github.io/masters2021/favicon-16x16.png"
+  },
+  {
+    "revision": "155715005fb7892a003cda4d94c56b8b",
+    "url": "https://zachjharris.github.io/masters2021/favicon-32x32.png"
+  },
+  {
+    "revision": "b10e741139d20b9d2df7956e0d505102",
+    "url": "https://zachjharris.github.io/masters2021/img/masters_logo.b10e7411.png"
+  },
+  {
+    "revision": "7c04119787c9bf30b8b94deed25ca5fb",
+    "url": "https://zachjharris.github.io/masters2021/index.html"
+  },
+  {
+    "revision": "9ebe689afa0a9e0e3798",
+    "url": "https://zachjharris.github.io/masters2021/js/app.6e2e50a7.js"
+  },
+  {
+    "revision": "20046d1d74a88670d29b",
+    "url": "https://zachjharris.github.io/masters2021/js/chunk-vendors.d9e73895.js"
+  },
+  {
+    "revision": "bfbefa3d51343298a171bb1871ecad86",
+    "url": "https://zachjharris.github.io/masters2021/logo.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "https://zachjharris.github.io/masters2021/logo.svg"
+  },
+  {
+    "revision": "15e0c3315a5e2727d9dd348367825762",
+    "url": "https://zachjharris.github.io/masters2021/manifest.json"
+  },
+  {
+    "revision": "b10e741139d20b9d2df7956e0d505102",
+    "url": "https://zachjharris.github.io/masters2021/masters_logo.png"
+  }
+]);
